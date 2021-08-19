1ca4a6714e315be477a1314081414ab3711c44ac
collinsMutai <collinsfrontend@gmail.com>
2021-08-19 05:37:55 +0300

    test

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from flask_wtf.file import FileField, FileAllowed
 
 
 
-from models import setup_db, Userinput
+from models import setup_db, Userinput, db_drop_and_create_all
 
 
 
@@ -21,8 +21,8 @@ def create_app(test_config=None):
     app = Flask(__name__)
     setup_db(app)
     CORS(app)
-    db = SQLAlchemy(app)
-    # migrate = Migrate(app, db)
+    db_drop_and_create_all
+
 
 
 
@@ -34,38 +34,40 @@ def create_app(test_config=None):
         response.headers.add("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS")
         return response
 
-
-    class Contact(FlaskForm):
-        name = StringField('Name')
-        phone = StringField('Phone')
-        message = StringField('message')
+    @app.route('/', methods=['GET'])
+    def home():
+        return jsonify({'message': 'Hello,hello, World!'})  
+    # class Contact(FlaskForm):
+    #     name = StringField('Name')
+    #     phone = StringField('Phone')
+    #     message = StringField('message')
    
 
-    @app.route('/', methods=['GET', 'POST'])
-    def index():
+    # @app.route('/', methods=['GET', 'POST'])
+    # def index():
 
-        form = Contact(request.form)
+    #     form = Contact(request.form)
 
-        if form.validate_on_submit():
+    #     if form.validate_on_submit():
    
-            name = form.name.data
-            phone = form.phone.data
-            message = form.message.data
+    #         name = form.name.data
+    #         phone = form.phone.data
+    #         message = form.message.data
 
-            newMessage = Userinput(
-                name=name,
-                phone=phone,
-                message=message,
-            )
-            db.session.add(newMessage)
+    #         newMessage = Userinput(
+    #             name=name,
+    #             phone=phone,
+    #             message=message,
+    #         )
+    #         db.session.add(newMessage)
 
-            db.session.commit()
+    #         db.session.commit()
             
 
 
 
 
-        return render_template('index.html', form=form)
+    #     return render_template('index.html', form=form)
 
 
 
@@ -105,19 +107,20 @@ if __name__ == '__main__':
     #  python manage.py db upgrade
 
     # heroku login
-    # heroku create flaskresumeapp
+    # heroku create flaskresume
 
-    # heroku addons:create heroku-postgresql:hobby-dev --app flaskresumeapp
+    # heroku addons:create heroku-postgresql:hobby-dev --app flaskresume
 
-    # heroku config --app flaskresumeapp
+    # heroku config --app flaskresume
 
 
-    # DATABASE_URL: postgres://rhllkkpsgvwvco:c86d4df4461a104453300c38e6c21121377cb5d3de3debb6e9aeec4c9144709a@ec2-52-202-152-4.compute-1.amazonaws.com:5432/dem1dl3hkb7hm0
+    # DATABASE_URL: postgres://xnpmwkvsflzwlk:72be8b649d12c2952ec87a2f65a73003d0438e06fe4151035337b71fd67855b4@ec2-54-147-93-73.compute-1.amazonaws.com:5432/d7khds4ruc6btv
+
 
 
     # deploy
     # git init
-    # heroku git:remote -a flaskresumeapp
+    # heroku git:remote -a flaskresume
     # git add .
     # git commit -m ""
     # git push heroku master
